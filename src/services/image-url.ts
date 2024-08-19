2b919988742718f3833c94d3fb13bffdca795506
Alexandre Gilbert <78440373+AGilbertDev@users.noreply.github.com>
2024-08-19 16:53:30 -0400

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,5 @@
 const getCroppedImageUrl = (url: string) => {
+	if (!url) return "";
 	const target = "media/";
 	const index = url.indexOf(target) + target.length; // Finds index right after "media/" in the url.
 	return url.slice(0, index) + "crop/600/400/" + url.slice(index); // Returns the url with the crop query inserted.
