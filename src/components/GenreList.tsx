65dbba58375e08e655fe201e05cbc9f9b6ca2504
Alexandre Gilbert <78440373+AGilbertDev@users.noreply.github.com>
2024-08-19 17:10:06 -0400

    Build search input

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, List, ListItem, Spinner, Button } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Button, Text } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 
 interface Props {
@@ -17,7 +17,9 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 					<HStack>
 						<Image boxSize="32px" borderRadius={8} src={genre.image_background}></Image>
 						<Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">
-							{genre.name}
+							<Text whiteSpace="wrap" textAlign="left">
+								{genre.name}
+							</Text>
 						</Button>
 					</HStack>
 				</ListItem>
