31286fdb1d4edd47226fd3de698bf3dc31c38488
Alexandre Gilbert <78440373+AGilbertDev@users.noreply.github.com>
2024-08-19 16:57:38 -0400

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-	if (!url) return "";
+	if (!url) return noImage;
 	const target = "media/";
 	const index = url.indexOf(target) + target.length; // Finds index right after "media/" in the url.
 	return url.slice(0, index) + "crop/600/400/" + url.slice(index); // Returns the url with the crop query inserted.
