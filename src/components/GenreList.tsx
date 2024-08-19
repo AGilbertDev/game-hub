f77e0d0d74c2b982f8f77559391343f9b9c58ef2
Alexandre Gilbert <78440373+AGilbertDev@users.noreply.github.com>
2024-08-19 13:48:17 -0400

    Highlight the selected genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,9 +3,10 @@ import useGenres, { Genre } from "../hooks/useGenres";
 
 interface Props {
 	onSelectGenre: (genre: Genre) => void;
+	selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
 	const { data, isLoading, error } = useGenres();
 	if (error) return null;
 	if (isLoading) return <Spinner />;
@@ -15,7 +16,7 @@ const GenreList = ({ onSelectGenre }: Props) => {
 				<ListItem key={genre.id} paddingY="5px">
 					<HStack>
 						<Image boxSize="32px" borderRadius={8} src={genre.image_background}></Image>
-						<Button onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">
+						<Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} onClick={() => onSelectGenre(genre)} fontSize="lg" variant="link">
 							{genre.name}
 						</Button>
 					</HStack>
